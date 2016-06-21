4795e68400de1effc99943bd3499697496d66e32
U-SANITAS\druizp <druizp@INTERNET4.sanitas.dom>
2016-06-21 13:44:48 +0000

    View 1 - some bindings

--- angular-seed/app/components/services/sharedService.js
+++ angular-seed/app/components/services/sharedService.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('')
+        .module('myApp')
         .service('sharedService', sharedService);
 
     /*@ngInject*/
--- angular-seed/app/view1/view1.js
+++ angular-seed/app/view1/view1.js
@@ -5,11 +5,18 @@ angular.module('myApp.view1', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
+    controller: 'View1Ctrl as vm'
   });
 }])
 
+
 .controller('View1Ctrl', ['sharedService', function(sharedService) {
-	var sharedValueOnController = sharedService.getSharedValue();
-	var defaultValue = "This is a default value";
+	
+	this.sharedValueOnController = sharedService.getSharedValue();
+	this.defaultValue = "This is a default value";
+	this.changeSharedValue = changeSharedValue;
+
+	function changeSharedValue(newValue) {
+		sharedService.setSharedValue(newValue);
+	}
 }]);
